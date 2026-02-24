6e5239f117b558197e4f881920da9bf2ae4c5c73
Seva <shukurovsiyovush1@gmail.com>
2026-02-24 14:25:37 +0500

    Update test_genfiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,17 +1,18 @@
-
-
 from gendiff.scripts.gendiff import generate_diff
 
 
-def test_base():
+def test_base(capsys):
 
-    result = generate_diff('file1.json', 'file2.json')
+    generate_diff('file1.json', 'file2.json')
+
+    result = capsys.readouterr()
     
-    assert result == '''{
+    assert result.out == '''{
   - follow: false
     host: hexlet.io
   - proxy: 123.234.53.22
   - timeout: 50
   + timeout: 20
   + verbose: true
-}'''
+}
+'''
