c70578bc1b59b090aba08320dd100a5319b8bed0
Seva <shukurovsiyovush1@gmail.com>
2026-02-26 11:43:54 +0500

    Add tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,18 +1,55 @@
-from gendiff.scripts.gendiff import generate_diff
+from pathlib import Path
+
+from gendiff.formats.gendiff import generate_diff
+from gendiff.scripts.gendiff import _get_data_path
 
 
 def test_base(capsys):
 
-    generate_diff('file1.json', 'file2.json')
+    generate_diff(_get_data_path('file1.json'), _get_data_path('file2.json'))
 
     result = capsys.readouterr()
+
+    correct = Path(__file__).parent / "test_data" / 'correct.txt'
+
+    assert result.out == open(correct, 'r', encoding='utf-8').read()
+
+
+def test_empty(capsys):
+
+    generate_diff(_get_data_path('file1.json'), _get_data_path('empty.json'))
+
+    result = capsys.readouterr()
+
+    correct = Path(__file__).parent / "test_data" / 'empty_correct.txt'
+
+    assert result.out == open(correct, 'r', encoding='utf-8').read()
+
+
+def test_error_file(capsys):
     
-    assert result.out == '''{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}
-'''
+    generate_diff(_get_data_path('file1.json'), _get_data_path('error.json'))
+
+    result = capsys.readouterr()
+
+    assert result.out == 'Incorrect JSON file uploaded'
+
+
+def test_reverse(capsys):
+
+    generate_diff(_get_data_path('file2.json'), _get_data_path('file1.json'))
+
+    result = capsys.readouterr()
+
+    correct = Path(__file__).parent / "test_data" / 'correct_reverse.txt'
+
+    assert result.out == open(correct, 'r', encoding='utf-8').read()
+
+
+def test_missing_file(capsys):
+    
+    generate_diff(_get_data_path('missing.json'), _get_data_path('file2.json'))
+
+    result = capsys.readouterr()
+
+    assert result.out == 'File missing.json not found!'
