bf8944671e58b2b6b96c1ec8bcbc258dec9fb8a2
Seva <shukurovsiyovush1@gmail.com>
2026-03-10 11:14:21 +0500

    Fix engine.py

--- gendiff/engine.py
+++ gendiff/engine.py
@@ -87,6 +87,7 @@ def generate_diff(
             return
         
         print(formated_diff, end='')
+        return formated_diff
 
     elif file_extention == 'yaml':
         formated_diff = load_yaml(
@@ -99,6 +100,7 @@ def generate_diff(
             return
         
         print(formated_diff, end='')
+        return formated_diff
 
     else:
         print('Incorrect file format or loaded files with different extentions',
